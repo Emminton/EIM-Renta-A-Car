63b10d5832a4c1aa0c32f1bbf57e879d5e8754b7
Martin Eduardo Camilo De La Cruz <martin_camilo@ucne.edu.do>
2020-07-21 08:27:16 -0400

    Paginacion en cliente terminado

---
+++ EIMRentaaCar/Models/Paginacion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EIMRentaaCar.Models
+{
+    public class Paginacion
+    {
+        public int Pagina { get; set; }
+        public int Mostrar { get; set; }
+        public Paginacion()
+        {
+            Pagina = 1;
+            Mostrar = 2;
+        }
+    }
+}
