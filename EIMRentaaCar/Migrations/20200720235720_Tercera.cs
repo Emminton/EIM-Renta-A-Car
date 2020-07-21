09a9e16a5a668869152686e0024b3beaa4449669
Martin Eduardo Camilo De La Cruz <martin_camilo@ucne.edu.do>
2020-07-20 20:22:59 -0400

    Notificaciones completa al 80%

---
+++ EIMRentaaCar/Migrations/20200720235720_Tercera.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EIMRentaaCar.Migrations
+{
+    public partial class Tercera : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notificaciones",
+                columns: table => new
+                {
+                    NotificacionId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UsuarioId = table.Column<int>(nullable: false),
+                    Titulo = table.Column<string>(nullable: false),
+                    Asunto = table.Column<string>(nullable: false),
+                    Fecha = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notificaciones", x => x.NotificacionId);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Usuarios",
+                keyColumn: "UsuarioId",
+                keyValue: 1,
+                column: "FechaIngreso",
+                value: new DateTime(2020, 7, 20, 19, 57, 20, 244, DateTimeKind.Local).AddTicks(9222));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notificaciones");
+
+            migrationBuilder.UpdateData(
+                table: "Usuarios",
+                keyColumn: "UsuarioId",
+                keyValue: 1,
+                column: "FechaIngreso",
+                value: new DateTime(2020, 7, 20, 19, 21, 35, 219, DateTimeKind.Local).AddTicks(7285));
+        }
+    }
+}
